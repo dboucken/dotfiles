78c69271ed7bea79570916d05f74f158dbdccfe3
dboucken <david.bouckenhove@nokia.com>
2021-09-23 11:02:47 +0200

    Revert "cleanup"
    This reverts commit fea13f948e6ee390f74bb0752bcdff578b4e3255.

--- install.sh
+++ install.sh
@@ -12,11 +12,14 @@ ln -sv $PWD/tmux.conf $HOME/.tmux.conf
 ln -sv $PWD/vimrc $HOME/.vimrc
 ln -sv $PWD/init.lua $HOME/.config/nvim/init.lua
 
+# Install base16-shell
+rm -rf ~/.config/base16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
 # Source bashrc
 echo "source ${PWD}/bashrc" >> $HOME/.bashrc
 
 # Install vim plugins
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall" -c "qa"
 
 # Install neovim plugins
--- uninstall.sh
+++ uninstall.sh
@@ -11,10 +11,13 @@ rm -v $HOME/.config/nvim/init.lua
 # Remove bashrc
 sed -i '/source.*bashrc/d' $HOME/.bashrc
 
+# Remove base16-shell
+rm -rf ~/.base16_theme
+rm -rf ~/.vimrc_background
+rm -rf ~/.config/base16-shell/
+
 # Remove Neovim plugins
 rm -rf ~/.local/share/nvim/site/pack/packer/
 
 # Remove created directories
-rm -rf $HOME/.vim/autoload
-rm -rf $HOME/.vim/plugged
 rm -rf $HOME/.vim/undo
